8ab7df5094d25f7dc8b7ee7dbbf841ab16db82b8
Tony Xue <tonyxue@users.noreply.github.com>
2014-03-16 13:35:25 +0800

    屏蔽了好友照片挖坟功能

--- renren_cleanup.user.js
+++ renren_cleanup.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name          人人网去广告 (Renren Cleanup)
 // @namespace     http://userstyles.org
-// @description	  简化人人网主页及其他页面，去除了包括广告，各种烦人的提示和多余的元素，简化了边栏。2014-02-24
-// @author        James Liu
-// @homepage      http://userstyles.org/styles/87253/renren-cleanup
+// @description	  简化人人网主页及其他页面，去除了包括广告，各种烦人的提示和多余的元素，简化了边栏。 2013-05-12
+// @author        Nobody
+// @homepage      https://github.com/tonyxue/renren_cleanup
 // @include       http://renren.com/*
 // @include       https://renren.com/*
 // @include       http://*.renren.com/*
@@ -11,7 +11,7 @@
 // @run-at        document-start
 // ==/UserScript==
 (function() {
- var css = "#ad100000000100,#ad1000000060,#ad1000000065,.side-item,.today-topic,.vip-level,.user-info-vip,#appDynamic,.dressup-home-btn,#apps-count,.apps-item-count,.rx-timeline-con,.wp-rrzm-popup,.ad-button,.main-recommend,#showAppMenu,.pv-recommentfriend-box,.Widget,.p-picSlide-cont,.downloadclient,.rrgame,.kfc-side,.share-help,.app-center-popup,#ugc-suggest-btn,.share-quick,.ad_rx,#zidou_music,#cleanFlashContent,#friendPhoto,.login-recommend,.friend-banner,#tools-box,.right_side_div,.sy_box,.right_side_div,#notice_system,#right_side_div,#banner-strach,.xb-qixi-footer,#rightapp_side_div,.xb-cornerspread-footer,.site-menu-apps-locked,.xb-bud-footer,#left-menu-pop-app,#ad_box,.xb-rightspread-footer{display:none !important;}.footprint,.birthday,.more-blogs,#friendPhotoCon{display:block !important;}.tip-arrow-top{right:70px !important;}";
+ var css = "#ad100000000100,#ad1000000060,#ad1000000065,#ad1000000093,#ad100000100001,.side-item,.today-topic,.vip-level,.user-info-vip,.fp-container,#appDynamic,.dressup-home-btn,#apps-count,.apps-item-count,.rx-timeline-con,.wp-rrzm-popup,.ad-button,.main-recommend,#showAppMenu,.pv-recommentfriend-box,.Widget,.p-picSlide-cont,.downloadclient,.rrgame,.kfc-side,.share-help,.app-center-popup,#ugc-suggest-btn,.share-quick,.ad_rx{display:none !important;}\n.footprint,.birthday{display:block !important;}\n.tip-arrow-top{right: 70px !important;}\n.ready-to-fix{display:none !important;}";
 if (typeof GM_addStyle != "undefined") {
 	GM_addStyle(css);
 } else if (typeof PRO_addStyle != "undefined") {
